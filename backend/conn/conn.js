dea788b73882101b1b3c8cd0da82561bb90da6fc
Nishar <neesharansari12341@gmail.com>
2024-08-25 23:55:18 +0530

    add string db

--- backend/conn/conn.js
+++ backend/conn/conn.js
@@ -4,7 +4,8 @@ const conn =async(req, res) => {
 try {
     
 
-        await mongoose.connect ("mongodb://localhost:27017/test").then(()=>{
+        // await mongoose.connect ("mongodb://localhost:27017/test").then(()=>{
+        await mongoose.connect ("mongodb+srv://neesharansari12341:hPH820U81ZOTSsNG@test.vvemymo.mongodb.net/?retryWrites=true&w=majority&appName=test").then(()=>{
     
             console.log("connected")
         });
